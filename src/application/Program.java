5dd75918208ad9b86019eb1bf0721b16969cff3d
Uanderson Santana <udsantos@indracompany.com>
2022-05-05 10:14:52 -0300

    Implementação do metodo de inserção e atualização.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -17,19 +18,23 @@ public class Program {
 	  Seller seller = sellerDao.findById(3);
 	  System.out.println(seller);
 	  
-	  System.out.println("=== TESTE 2: seller findByDepartment =====");
+	  System.out.println("\n=== TESTE 2: seller findByDepartment =====");
 	  Department department = new Department(2,null);
 	  List<Seller> listSeller = sellerDao.findByDepartment(department);
 	  for (Seller obj: listSeller) {
 		  System.out.println(obj);
 	  }
 	  
-	  System.out.println("=== TESTE 3: seller findAll =====");
+	  System.out.println("\n=== TESTE 3: seller findAll =====");
 	  listSeller = sellerDao.findAll();
 	  for (Seller obj: listSeller) {
 		  System.out.println(obj);
-	  }	  
+	  }	
 	  
+	  System.out.println("\n=== TESTE 4: seller insert =====");
+	  Seller newSeller = new Seller(null, "Uanerson Santana", "uanderson21@gmail.com", new Date(), 15000.0, department);
+	  sellerDao.insert(newSeller); 
+	  System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
